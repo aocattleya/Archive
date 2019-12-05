c8fe41c0611145c844ef3652374a9d3e7125bbf4
jku0704 <newground.nufm@gmail.com>
2019-12-05 21:17:15 +0900

    front pade wrote
    I coded fornt page by HAML.(not still coded SASS)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
+  #root 'items#index'
+  root 'items#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

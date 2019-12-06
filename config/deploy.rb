9d2b92e97c2043d5c4003ff8fe6713ca977293a2
aocattleya <aocattleya@yahoo.co.jp>
2019-12-06 12:17:02 +0900

    fix: 公開鍵のpath指定を正しく修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/deploy.pem）>']
+                  keys: ['~/.ssh/deploy.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }

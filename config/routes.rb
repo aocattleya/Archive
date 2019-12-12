894af48a4109bb86ebb8a26323b719f47c38a134
jku0704 <newground.nufm@gmail.com>
2019-12-12 16:43:04 +0900

    add: category

--- config/routes.rb
+++ config/routes.rb
@@ -22,11 +22,13 @@ Rails.application.routes.draw do
   resources :items do
     collection do
       get :confirm
+      get :category
     end
   end
 
   devise_for :users
   root 'items#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

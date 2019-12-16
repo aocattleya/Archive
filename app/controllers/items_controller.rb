35c017a6d42e3c7ea96c7564eba3eb110c15b470
mstbeat <mrbrightside82@ymail.com>
2019-12-16 20:53:06 +0900

    add: 商品情報編集の実装（画像以外）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,36 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    # @brand = Brand.find(@item.brand_id)
+    @parents = Category.all.order("id ASC").limit(13)
+    # @postages = Postage.all.order("id ASC").limit(2)
+    # gon.item = @item
+    # gon.brand = @brand
+    # gon.category = @category
+    img_array = []
+    @item.images.each do |image|
+      img_array.push(image)
+    end
+    # gon.img_array = img_array
+  end
+
+  def update
+    @item = Item.find(params[:id])
+      # if num_params[:num] != nil
+      #   num_params[:num].each do |n|
+      #     n = n.to_i
+      #     @item.images[n].purge
+      #   end
+      # end
+      # if image_params[:images] != nil
+      #   @item.update(image_params)
+      # end
+    @item.update(item_params)
+    redirect_to item_path
+  end
+
   private
     def item_params
       params.require(:item).permit(:name, :description, :price, :size, :category_id, :condition, :shipping_date, :shipping_price, :shipping_area, :shipping_method, :category_id, :brand_id, :user_id, images_attributes: {image: []})

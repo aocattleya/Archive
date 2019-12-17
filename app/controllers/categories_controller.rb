9ccc0f504ddca0e70006fc61a76e9ebc98e0aef2
mstbeat <mrbrightside82@ymail.com>
2019-12-17 16:17:57 +0900

    update: コントローラーとビューの修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,23 +5,8 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    if @category.children.length == 0
-      @items = Item.where(category_id: params[:id])
-      return
-    end
-
-    categories = @category.children
-    ids = []
-    categories.each do |item|
-      id = item.children.pluck(:id)
-      id.each do |i|
-        ids.push(i)
-      end
-    end
-    if ids.length == 0
-      ids = @category.children.pluck(:id)
-      @items = Item.where(category_id: ids)
-    end
+    @items = Item.where(category_id: params[:id]) if @category.children.length == 0
+    ids = @category.children.pluck(:id)
     @items = Item.where(category_id: ids)
   end
 end

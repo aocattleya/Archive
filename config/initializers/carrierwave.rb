d037f79921f640a50d95bd7cc1be14d2a11398cf
mstbeat <mrbrightside82@ymail.com>
2019-12-17 15:53:34 +0900

    update: コメントアウト部分の削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,5 +14,4 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'mercariimages'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercariimages'
-  # config.asset_host = 'https://mercariimages.s3.amazonaws.com' これが正しい？
 end

e5f3399c4bfe3ad9dfb0266addffe72e0edb3a6b
aocattleya <aocattleya@yahoo.co.jp>
2019-12-18 21:00:59 +0900

    update: 商品詳細表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
 
   def index
     @items = Item.all

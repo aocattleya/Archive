7d661bc4439024248506df5105d8a3c9383fdfdd
mstbeat <mrbrightside82@ymail.com>
2019-12-19 13:25:14 +0900

    update: 出品者名と発送元地域の表示を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,7 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     if @item.destroy
       redirect_to root_path
@@ -42,13 +43,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(@item.user_id)
+    @prefecture = Prefecture.find(@item.shipping_area)
+  end
+
   private
     def set_item
       @item = Item.find(params[:id])
     end
 
     def item_params
-      params.require(:item).permit(:name, :description, :price, :size, :category_id, :condition, :shipping_date, :shipping_price, :shipping_area, :shipping_method, :category_id, :brand_id, :user_id, images_attributes: :image)
+      params.require(:item).permit(:name, :description, :price, :size, :category_id, :condition, :shipping_date, :shipping_price, :shipping_area, :shipping_method, :category_id, :brand_id, :user_id, images_attributes: :image).merge(user_id: current_user.id)
     end
 
     def update_params

04f201c29b3c88d455a2c72f78fb0e04025ca78e
mstbeat <mrbrightside82@ymail.com>
2019-12-23 18:34:07 +0900

    update: 本人しかプロフィールページに行けないよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
   before_action :set_user_items, only: [:show, :listing]
+  before_action :valid_user, only: [:show, :listing]
+
   def show
+    redirect_to root_path unless @user == current_user
   end
 
   def listing
-  
+    redirect_to root_path unless @user == current_user
   end
 
   private
+
+    def valid_user
+      redirect_to root_path unless @user == current_user
+    end
+
     def set_user_items
       @user = User.find(params[:id])
       @items = Item.where(user_id: @user.id)
